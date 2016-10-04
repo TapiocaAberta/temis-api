e6e35d3c91d2a94e880d7a88217063758fa9a149
fabiohbarbosa <fabiohbarbosa@gmail.com>
2016-10-04 17:37:46 -0300

    Adicionando POI e commons-io

--- src/main/java/com/sjcdigital/temis/model/service/parsers/impl/PresenceAldermanParser.java
+++ src/main/java/com/sjcdigital/temis/model/service/parsers/impl/PresenceAldermanParser.java
@@ -1,17 +1,32 @@
 package com.sjcdigital.temis.model.service.parsers.impl;
 
 import com.sjcdigital.temis.model.service.parsers.AbstractParser;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 /**
  * @author fabiohbarbosa
  */
+@Component
 public class PresenceAldermanParser extends AbstractParser {
 
     @Override
     public void parse(final File file) {
-        // TODO fabiohbarbosa Implementar parse
+
+        try {
+            final Document document = readFile(file).get();
+
+            // TODO fabiohbarbosa pegar a data e vereadores vs. presença
+
+            System.out.println("");
+        } catch (InterruptedException | ExecutionException | IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
