4cc7ca9b24811e825cce2bc75b63ecb393477f3d
fabiohbarbosa <fabiohbarbosa@gmail.com>
2016-10-07 10:48:27 -0300

    Adicionado pesquisa por sessão

--- src/main/java/com/sjcdigital/temis/model/repositories/OrdinarySessionRepository.java
+++ src/main/java/com/sjcdigital/temis/model/repositories/OrdinarySessionRepository.java
@@ -1,10 +1,15 @@
 package com.sjcdigital.temis.model.repositories;
 
+import com.sjcdigital.temis.model.document.Alderman;
 import com.sjcdigital.temis.model.document.OrdinarySession;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 /**
  * @author fabiohbarbosa
  */
 public interface OrdinarySessionRepository extends MongoRepository<OrdinarySession, String> {
+    Optional<Alderman> findBySessionAndDateAndAlderman(int session, LocalDate date, Alderman alderman);
 }
