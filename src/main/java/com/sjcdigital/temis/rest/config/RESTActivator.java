a3d6cadf8cd513de794104ee16ef83936ec6ff2d
Pedro Silva <pesilva@pesilva.remote.csb>
2017-03-28 15:23:51 -0300

    versao JavaEE

---
+++ src/main/java/com/sjcdigital/temis/rest/config/RESTActivator.java
@@ -0,0 +1,11 @@
+package com.sjcdigital.temis.rest.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author pedro-hos
+ *
+ */
+@ApplicationPath("api/v2")
+public class RESTActivator extends Application { }
